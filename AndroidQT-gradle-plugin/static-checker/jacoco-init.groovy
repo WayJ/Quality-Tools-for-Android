b33a9aec9c1f3af03b664f24be02585438157833
jiangwei <jiangwei@hztianque.com>
2020-09-16 10:50:36 +0800

    Update jacoco-init.groovy

--- AndroidQT-gradle-plugin/static-checker/jacoco-init.groovy
+++ AndroidQT-gradle-plugin/static-checker/jacoco-init.groovy
@@ -52,8 +52,8 @@ void addJacocoTestReport(final Project project) {
 
         }else{
             sourceDirectories = files([MAIN_SRC])
+            lassDirectories = files([DEBUG_TREE])
         }
-        classDirectories = files([DEBUG_TREE])
         executionData = fileTree(dir:project.buildDir, includes:[
                 'jacoco/testDebugUnitTest.exec', 'outputs/code-coverage/connected/*coverage.ec', '../jacoco.exec',
         ])
